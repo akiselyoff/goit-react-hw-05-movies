9518c2d5c9d77a5887157c3fece54d8c0c05b0f5
akiselyoff <a.kiselyoff@gmail.com>
2022-06-28 20:18:20 +0000

    Deploying to gh-pages from @ akiselyoff/goit-react-hw-05-movies@ab4eb3637481ba38c5724050e2211bf5ffe784ee 🚀

---
+++ static/js/805.29df6636.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h1",{children:"Page Not Found"})})}}}]);
+//# sourceMappingURL=805.29df6636.chunk.js.map
